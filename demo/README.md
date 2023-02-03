a23cc11b418b307209fc789325849d46e2457f8a
Zdenek Hak <zdendahak@gmail.com>
2023-02-03 16:17:25 +0100

    basics

---
+++ demo/README.md
@@ -0,0 +1,7 @@
+# demo
+
+## db localhost
+http://localhost:8085/h2-console
+
+user:sa
+password:password
---
+++ demo/src/main/java/cz/zdenek/demo/rest/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package cz.zdenek.demo.rest.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
